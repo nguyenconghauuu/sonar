2ae8efaba01d9f0c99ded0bc886f0ab0bc53c8b5
nguyenconghauuu <115331673+nguyenconghauuu@users.noreply.github.com>
2022-12-02 08:18:59 +0700

    Update Banner.php

--- Banner.php
+++ Banner.php
@@ -25,7 +25,7 @@
                         </div>
                         <div class="image-title">
                                 <h1>this is demo silder</h1>
-                                <div class="desc">Với Piktochart, người dùng có thể tạo bài thuyết trình, áp phích, tở rơi quảng cáo, báo cáo, đồ họa thông tin, đồ họa truyền thông,</div>
+                                <div class="desc">Với Piktochart, người dùng có thể tạo bài thuyết trình</div>
                         </div>
                 </div>
                 <div class="image-item">
